8e4a68d1dae702ed478aa806f0d07debb3a37489
alrasyid.mirza.mm@gmail.com <alrasyid.mirza.mm@gmail.com>
2021-04-30 16:00:20 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [listOfPost, setListOfPost] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/posts').then((res) => {
+      setListOfPost(res.data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {listOfPost.map((value, key) => {
+        return (
+          <div className="post"> 
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
